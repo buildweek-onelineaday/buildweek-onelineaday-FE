9a0371b684050d694bd53aec9dc67724447e241c
ericlugo <ericlugo@users.noreply.github.com>
2019-07-17 00:04:42 -0400

    Added menu and toolbar to dashboard view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import LoginView from './views/LoginView';
-import Dashboard from './components/Dashboard';
-=======
 import { PrivateRoute } from './components';
-import LoginPage from './views/LoginPage';
+import LoginView from './views/LoginView';
 import Dashboard from './views/Dashboard';
->>>>>>> 1d72eb7b233f6306cb7d50b6053b87fb0c55c4b4
 
 const theme = createMuiTheme({
   palette: {
@@ -32,19 +27,10 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <Router>
-<<<<<<< HEAD
         {/* <Route exact path='/' component={LandingView} /> */}
-        <Route exact path='/login' component={LoginView} />
-        <Route exact path='/dashboard' component={Dashboard} />
-=======
-        {/* <Route exact path='/' component={LandingPage} /> */}
-        {/* <Route exact path='/login' component={LoginPage} /> */}
-        <Route exact path='/' component={LoginPage} />
-        <PrivateRoute
-          exact path="/dashboard"
-          component={Dashboard}
-        />
->>>>>>> 1d72eb7b233f6306cb7d50b6053b87fb0c55c4b4
+        {/* <Route exact path='/login' component={LoginView} /> */}
+        <Route exact path='/' component={LoginView} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
       </Router>
     </MuiThemeProvider>
   );
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,48 +1,222 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import { connect } from 'react-redux';
-
-import { styled } from '@material-ui/styles';
-
+import { makeStyles } from '@material-ui/core/styles';
 import {
-  addEntry,
-  deleteEntry
-} from '../store/actions';
-
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  Divider,
+  Button,
+  IconButton,
+  Container,
+  Link,
+} from '@material-ui/core';
 import {
-  EntryForm,
-  Timeline
-} from '../components/dashboard';
+  Menu as MenuIcon,
+  ChevronLeft as ChevronLeftIcon,
+  Dashboard as DashboardIcon,
+  BarChart as BarChartIcon,
+  Settings as SettingsIcon,
+  PowerSettingsNew as LogoutIcon,
+} from '@material-ui/icons';
 
-const Container = styled(`div`)({
-  padding: '0 12px'
-});
+import { addEntry, deleteEntry } from '../store/actions';
+import { EntryForm, Timeline } from '../components/dashboard/';
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <Container>
-        <EntryForm
-          addEntry={this.props.addEntry}
-        />
-        <Timeline
-          deleteEntry
-          entries={this.props.entries}
-        />
-      </Container>
-    );
-  }
+const mainListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary='Dashboard' />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary='10 Year Overview' />
+    </ListItem>
+  </div>
+);
+
+const secondaryListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <SettingsIcon />
+      </ListItemIcon>
+      <ListItemText primary='Settings' />
+    </ListItem>
+  </div>
+);
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    backgroundColor: theme.palette.primary.light,
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+  button: {
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
+
+function Dashboard(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='absolute' className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='Open drawer'
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component='h1' variant='h6' color='inherit' noWrap className={classes.title}>
+            Welcome to the One Line a Day Journal!
+          </Typography>
+          <Button variant='contained' color='inherit' className={classes.button}>
+            <LogoutIcon />
+            Log Out
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant='permanent'
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth='lg' className={classes.container}>
+          <EntryForm addEntry={props.addEntry} />
+          <Timeline deleteEntry entries={props.entries} />
+        </Container>
+      </main>
+    </div>
+  );
 }
 
-const mapState = state => ({
-  entries: state.entries
+Dashboard.propTypes = {
+  //datatype validation goes here for provided props
+};
+
+const mapStateToProps = (state) => ({
+  entries: state.entries,
 });
 
 const mapDispatch = {
   addEntry,
-  deleteEntry
+  deleteEntry,
 };
 
 export default connect(
-  mapState,
-  mapDispatch
+  mapStateToProps,
+  mapDispatch,
 )(Dashboard);
