281e60306c592b2325cf08dcc23a4199c961e30b
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-07-20 13:01:56 -0400

    Fix fudged PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import ls from 'local-storage';
 
-export default ({ component: Component, ...otherProps }) => {
-  const token = ls.get('userToken');
-
-  if (!token) return <Redirect to='/' />;
-
-  return <Route {...otherProps} render={(props) => <Component {...props} />} />;
-};
+export default ({ component: Component, ...otherProps }) => (
+  <Route
+    {...otherProps}
+    render={props => 
+      ls.get('userToken') ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
